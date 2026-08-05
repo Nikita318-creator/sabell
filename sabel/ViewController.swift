59877e3831d8dc7e39682717d3b9e5f7d7849c43
mikitka <nikuvar77@gmail.com>
2026-08-06 00:25:20 +0300

    init

---
+++ sabel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sabel
+//
+//  Created by Mikita on 06/08/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
