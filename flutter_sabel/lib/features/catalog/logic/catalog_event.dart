55d77c9e6e78786e295fb5edefe0aae2d176f438
mikitka <nikuvar77@gmail.com>
2026-09-07 13:24:02 +0300

    push

---
+++ flutter_sabel/lib/features/catalog/logic/catalog_event.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/foundation.dart';
+
+@immutable
+abstract class CatalogEvent {
+  const CatalogEvent();
+}
+
+class LoadCatalogDataEvent extends CatalogEvent {
+  const LoadCatalogDataEvent();
+}
+
+class CatalogPullToRefreshEvent extends CatalogEvent {
+  const CatalogPullToRefreshEvent();
+}
+
+class SelectGalleryImageEvent extends CatalogEvent {
+  final int index;
+  const SelectGalleryImageEvent(this.index);
+}
+
+class CatalogAppResumedEvent extends CatalogEvent {
+  const CatalogAppResumedEvent();
+}
